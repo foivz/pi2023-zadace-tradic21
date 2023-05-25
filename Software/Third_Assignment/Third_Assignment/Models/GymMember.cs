089da704b4d18505de35b8edc51e6856da2addb6
Tea Radić <tradic21@foi.hr>
2023-05-25 12:40:19 +0200

    Implementirana klasa GymMember

--- Software/Third_Assignment/Third_Assignment/Models/GymMember.cs
+++ Software/Third_Assignment/Third_Assignment/Models/GymMember.cs
@@ -6,17 +6,14 @@ using System.Threading.Tasks;
 
 namespace Third_Assignment.Models
 {
-    public class GymMember
+    public abstract class GymMember
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string Gender { get; set; }
-        public string BirthDate { get; set; }
-        public Int64 Phone { get; set; }
+        public int Phone { get; set; }
         public string Email { get; set; }
-        public string JoinDate { get; set; }
-        public string Address { get; set; }
+        public string Adress { get; set; }
         public string MembershipStatus { get; set; }
 
     }
