25ea14563110d94a808e96c9ed64475a29fb159b
Tea Radić <tradic21@foi.hr>
2023-05-25 18:10:15 +0200

    Implementirana metoda za pretraživanje članova po Id

--- Software/Third_Assignment/Third_Assignment/FrmDashboard.Designer.cs
+++ Software/Third_Assignment/Third_Assignment/FrmDashboard.Designer.cs
@@ -28,37 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dgvGymMembers = new System.Windows.Forms.DataGridView();
+            this.lblEnterId = new System.Windows.Forms.Label();
+            this.txtEnterId = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnSearch = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvGymMembers)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvGymMembers
             // 
             this.dgvGymMembers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvGymMembers.Location = new System.Drawing.Point(36, 36);
+            this.dgvGymMembers.Location = new System.Drawing.Point(41, 70);
             this.dgvGymMembers.Name = "dgvGymMembers";
             this.dgvGymMembers.RowHeadersWidth = 62;
             this.dgvGymMembers.RowTemplate.Height = 28;
-            this.dgvGymMembers.Size = new System.Drawing.Size(715, 344);
+            this.dgvGymMembers.Size = new System.Drawing.Size(879, 261);
             this.dgvGymMembers.TabIndex = 0;
             this.dgvGymMembers.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvGymMembers_CellContentClick);
+            // 
+            // lblEnterId
+            // 
+            this.lblEnterId.AutoSize = true;
+            this.lblEnterId.Location = new System.Drawing.Point(37, 25);
+            this.lblEnterId.Name = "lblEnterId";
+            this.lblEnterId.Size = new System.Drawing.Size(86, 20);
+            this.lblEnterId.TabIndex = 1;
+            this.lblEnterId.Text = "Unesite Id:";
+            // 
+            // txtEnterId
+            // 
+            this.txtEnterId.Location = new System.Drawing.Point(130, 25);
+            this.txtEnterId.Name = "txtEnterId";
+            this.txtEnterId.Size = new System.Drawing.Size(157, 26);
+            this.txtEnterId.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(311, 24);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(85, 27);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Pretraži";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click_1);
             // 
             // FrmDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(942, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtEnterId);
+            this.Controls.Add(this.lblEnterId);
             this.Controls.Add(this.dgvGymMembers);
             this.Name = "FrmDashboard";
             this.Text = "Dashboard";
             this.Load += new System.EventHandler(this.Dashboard_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvGymMembers)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvGymMembers;
+        private System.Windows.Forms.Label lblEnterId;
+        private System.Windows.Forms.TextBox txtEnterId;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnSearch;
     }
 }
--- Software/Third_Assignment/Third_Assignment/FrmDashboard.cs
+++ Software/Third_Assignment/Third_Assignment/FrmDashboard.cs
@@ -9,6 +9,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using DBLayer;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Third_Assignment
 {
@@ -33,6 +36,52 @@ namespace Third_Assignment
         private void dgvGymMembers_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            
+                
+            
+        }
+
+        private void btnSearch_Click_1(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEnterId.Text))
+            {
+                int memberId;
+                // Pokušajte pretvoriti uneseni tekst u identifikacijski broj
+                if (int.TryParse(txtEnterId.Text, out memberId))
+                {
+                    // Dohvati samo jednog člana teretane na temelju identifikacijskog broja
+                    GymMember gymMember = GymMemberRepository.GetGymMember(memberId);
+
+                    // Provjerite je li pronađen član teretane s tim identifikacijskim brojem
+                    if (gymMember != null)
+                    {
+                        // Kreirajte novu listu s samo jednim članom teretane
+                        List<GymMember> singleMemberList = new List<GymMember> { gymMember };
+
+                        // Postavite izvor podataka za DataGridView na novu listu s jednim članom teretane
+                        dgvGymMembers.DataSource = singleMemberList;
+                    }
+                    else
+                    {
+                        // Član teretane nije pronađen s unesenim identifikacijskim brojem
+                        MessageBox.Show("Član teretane s tim identifikacijskim brojem nije pronađen.");
+                    }
+                }
+                else
+                {
+                    // Neispravan format identifikacijskog broja
+                    MessageBox.Show("Uneseni identifikacijski broj nije valjan.");
+                }
+            }
+            else
+            {
+                // Tekstualno polje za pretraživanje je prazno
+                MessageBox.Show("Unesite identifikacijski broj za pretraživanje.");
+            }
         }
     }
 }
